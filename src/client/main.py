33dc37d13710a6c88402aabcc806b3db7d44d757
Emil Boot <enemieltje@gmail.com>
2025-03-08 16:52:58 +0100

    readd sensor

--- src/client/main.py
+++ src/client/main.py
@@ -50,23 +50,26 @@ t_old = time.process_time()
 (vx, vy, vz) = (0, 0, 0)
 (x, y, z) = (0, 0, 0)
 while True:
-    # t = time.process_time()
-    # dt = t - t_old
-    # t_old = t
+    t = time.process_time()
+    dt = t - t_old
+    t_old = t
 
-    # (ax, ay, az) = sensor.linear_acceleration
-    # # (vx, vy, vz) = (vx + (ax * dt), vy + (ay * dt), vz + (az * dt))
-    # # (x, y, z) = (x + (0.5 * ax * dt * dt), y +
-    # #              (0.5 * ay * dt * dt), z + (0.5 * az * dt * dt))
-    # (mx, my, mz) = sensor.magnetic
+    (ax, ay, az) = sensor.linear_acceleration
+    # (vx, vy, vz) = (vx + (ax * dt), vy + (ay * dt), vz + (az * dt))
+    # (x, y, z) = (x + (0.5 * ax * dt * dt), y +
+    #              (0.5 * ay * dt * dt), z + (0.5 * az * dt * dt))
+    (mx, my, mz) = sensor.magnetic
 
-    # print("\033[1A\x1b[2K"*4)
-    # print(f"t: {t}, dt: {dt:1.3}")
-    # print(f"a: {ax:6.1}, {ay:6.1}, {az:6.1}")
-    # # print(f"v: {vx:6.1}, {vy:6.1}, {vz:6.1}")
-    # # print(f"pos: {x:6.1}, {y:6.1}, {z:6.1}")
-    # print(f"mag: {mx:4.2}, {my:4.2}, {mz:4.2}")
+    print("\033[1A\x1b[2K"*4)
+    print(f"t: {t}, dt: {dt:1.3}")
+    print(f"a: {ax:6.1}, {ay:6.1}, {az:6.1}")
+    # print(f"v: {vx:6.1}, {vy:6.1}, {vz:6.1}")
+    # print(f"pos: {x:6.1}, {y:6.1}, {z:6.1}")
+    print(f"mag: {mx:4.2}, {my:4.2}, {mz:4.2}")
     # # time.sleep(1)
+    data["MX"] = mx
+    data["MY"] = my
+    data["MZ"] = mz
 
     for name, channel in channels.items():
         data[name] -= rolling_averages[name][ri]
