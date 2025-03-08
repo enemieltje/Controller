6ee44f25a901f83dae8f57ff6ec5901c18e18a0b
Emil Boot <enemieltje@gmail.com>
2025-03-08 11:09:37 +0100

    typo

--- src/client/main.py
+++ src/client/main.py
@@ -44,7 +44,7 @@ while True:
             (channel.value * 2**16) / ri_max * 2)
         data[name] += rolling_averages[name][ri]
         ri = (ri + 1) % ri_max
-    data["ABS_Z"] = int(data["ABS_Z"]*3/10)
+    data["ABS_Z"] = math.floor(data["ABS_Z"]*3/10)
 
     try:
         requests.post('http://192.168.2.50:8080/uinput/emit',
